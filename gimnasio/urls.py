f5904bc701556fd0c7bdf11d78d039321fbd3f25
AngelRs12 <162938457+AngelRs12@users.noreply.github.com>
2026-01-14 00:07:09 -0600

    Update urls.py

--- gimnasio/urls.py
+++ gimnasio/urls.py
@@ -23,9 +23,3 @@ urlpatterns = [
     path('', include('appGym.urls')),
     path('admin/', admin.site.urls),
 ]
-
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
-
